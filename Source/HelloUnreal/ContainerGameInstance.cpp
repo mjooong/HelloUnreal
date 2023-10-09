3f712f7db6602d527b97502f0edefc6b3c048b5e
MinjongKim <minjong0524@gmail.com>
2023-10-09 17:11:22 +0900

    Unreal Container Library Array & Set & Struct & Map

--- Source/HelloUnreal/ContainerGameInstance.cpp
+++ Source/HelloUnreal/ContainerGameInstance.cpp
@@ -4,6 +4,21 @@
 #include "ContainerGameInstance.h"
 #include "Algo/Accumulate.h"
 
+FString MakeRandomName()
+{
+	TCHAR FirstChar[] = TEXT("김이박최");
+	TCHAR MiddleCharp[] = TEXT("상해지성");
+	TCHAR LastChar[] = TEXT("수은원연");
+
+	TArray<TCHAR> RandArray;
+	RandArray.SetNum(3);
+	RandArray[0] = FirstChar[FMath::RandRange(0, 3)];
+	RandArray[1] = MiddleCharp[FMath::RandRange(0, 3)];
+	RandArray[2] = LastChar[FMath::RandRange(0, 3)];
+
+	return RandArray.GetData();
+}
+
 void UContainerGameInstance::Init()
 {
 	Super::Init();
@@ -70,5 +85,77 @@ void UContainerGameInstance::Init()
 	Int32Set.Add(8);
 	Int32Set.Add(10);
 
+	/**
+	Struct
+	*/
+	const int32 StudentNum = 300;
+	for (int32 ix = 1; ix <= StudentNum; ++ix)
+	{
+		StudentData.Emplace(FStudentData(MakeRandomName(), ix));
+	}
+
+	TArray<FString> AllStudentsNames;
+	Algo::Transform(StudentData, AllStudentsNames, [](const FStudentData& Val)
+		{
+			return Val.Name;
+		});
+
+	UE_LOG(LogTemp, Log, TEXT("모든 학생 이름의 수 : %d"), AllStudentsNames.Num());
+
+	TSet<FString> AllUniqueNames;
+	Algo::Transform(StudentData, AllUniqueNames, [](const FStudentData& Val)
+		{
+			return Val.Name;
+		});
+
+	UE_LOG(LogTemp, Log, TEXT("중복 없는 학생 이름의 수 : %d"), AllUniqueNames.Num());
+
+	Algo::Transform(StudentData, StudentMap, [](const FStudentData& Val)
+		{
+			return TPair<int32, FString>(Val.Order, Val.Name);
+		});
+
+	UE_LOG(LogTemp, Log, TEXT("순번에 따른 학생 맵의 레코드 수 : %d"), StudentMap.Num());
+
+	/**
+	TMap
+	 중복 불허
+	 키 밸류 관리
+	*/
+	TMap<FString, int32> StudentsMapByUniqueName;
+
+	Algo::Transform(StudentData, StudentsMapByUniqueName, [](const FStudentData& Val)
+		{
+			return TPair<FString, int32>(Val.Name, Val.Order);
+		});
+
+	UE_LOG(LogTemp, Log, TEXT("이름에 따른 학생 맵의 레코드 수 : %d"), StudentsMapByUniqueName.Num());
+
+	/**
+	TMultiMap
+	 중복 허용
+	 키 밸류 관리
+	*/
+	TMultiMap<FString, int32> StudentsMapByName;
+
+	Algo::Transform(StudentData, StudentsMapByName, [](const FStudentData& Val)
+		{
+			return TPair<FString, int32>(Val.Name, Val.Order);
+		});
+
+	UE_LOG(LogTemp, Log, TEXT("이름에 따른 학생 멀티맵의 레코드 수 : %d"), StudentsMapByName.Num());
+
+	const FString TargetName(TEXT("김상수"));
+	TArray<int32> AllOrders;
+	StudentsMapByName.MultiFind(TargetName, AllOrders);
+
+	UE_LOG(LogTemp, Log, TEXT("이름이 %s인 학생 수 : %d"), *TargetName, AllOrders.Num());
+
+
+	TSet<FStudentData> StudentsSet;
+	for (int32 ix = 1; ix <= StudentNum; ++ix)
+	{
+		StudentsSet.Emplace(FStudentData(MakeRandomName(), ix));
+	}
 
 }
--- Source/HelloUnreal/ContainerGameInstance.h
+++ Source/HelloUnreal/ContainerGameInstance.h
@@ -1,10 +1,40 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
 
 #include "CoreMinimal.h"
 #include "Engine/GameInstance.h"
 #include "ContainerGameInstance.generated.h"
+
+USTRUCT()
+struct FStudentData 
+{
+	GENERATED_BODY()
+	
+	FStudentData()
+	{
+		Name = TEXT("홍길동");
+		Order = -1;
+	}
+
+	FStudentData(FString InName, int32 InOrder) : Name(InName), Order(InOrder) {}
+
+	bool operator==(const FStudentData& InOther) const
+	{
+		return Order == InOther.Order;
+	}
+
+	friend FORCEINLINE uint32 GetTypeHash(const FStudentData& InStudentData)
+	{
+		return GetTypeHash(InStudentData.Order);
+	}
+
+	UPROPERTY()
+	FString Name;
+
+	UPROPERTY()
+	int32 Order;
+};
 
 /**
  * 
@@ -14,9 +44,18 @@ class HELLOUNREAL_API UContainerGameInstance : public UGameInstance
 {
 	GENERATED_BODY()
 	
+	
 public:
 
 	virtual void Init() override;
 
-	
+private:
+	TArray<FStudentData> StudentData;
+
+	UPROPERTY()
+	TArray<TObjectPtr<class UStruct>> Students;
+
+	TMap<int32, FString> StudentMap;
+
+
 };
