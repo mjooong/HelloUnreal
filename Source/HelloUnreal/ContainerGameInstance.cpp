968664b6e2487556b9c28cfdd0ce564d41fa04a3
MinjongKim <minjong0524@gmail.com>
2023-10-09 15:56:01 +0900

    Container Library Array & Set

---
+++ Source/HelloUnreal/ContainerGameInstance.cpp
@@ -0,0 +1,74 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ContainerGameInstance.h"
+#include "Algo/Accumulate.h"
+
+void UContainerGameInstance::Init()
+{
+	Super::Init();
+
+
+	/** 
+	TSet
+	빈틈없는 메모리 제공
+	가장 높은 접근성능
+	가장 높은 순회성능
+	*/
+	const int32 ArrayNum = 10;
+	TArray<int32> Int32Array;
+
+	for (int32 ix = 1; ix <= ArrayNum; ++ix)
+	{
+		Int32Array.Add(ix);
+	}
+		
+	Int32Array.RemoveAll([](int32 Val)
+		{
+			return Val % 2 == 0;
+		});
+
+	Int32Array += {2, 4, 6, 8, 10};	// {1,3,5,7,9,2,4,6,8,10}
+
+	TArray<int32> Int32ArrayCompare;
+	int32 CArray[] = { 1,3,5,7,9,2,4,6,8,10 };
+	Int32ArrayCompare.AddUninitialized(ArrayNum);	// AddUninitialized : 초기화 되지 않는 데이터 더하기
+	FMemory::Memcpy(Int32ArrayCompare.GetData(), CArray, sizeof(int32) * ArrayNum); // FMemory::Memcpy 빠르게 복제
+
+	ensure(Int32Array == Int32ArrayCompare);
+
+	int32 Sum = 0;
+	for (const int32& Int32Elem : Int32Array)
+	{
+		Sum += Int32Elem;
+	}
+	ensure(Sum == 55);
+
+	int32 SumByAlgo = Algo::Accumulate(Int32Array, 0);
+	ensure(Sum == SumByAlgo);
+
+
+	/** 
+	TSet 
+	빠른 중복 감지
+	*/
+	TSet<int32> Int32Set;
+	for (int32 ix = 1; ix <= ArrayNum; ++ix)
+	{
+		Int32Set.Add(ix);
+	}
+
+	Int32Set.Remove(2);
+	Int32Set.Remove(4);
+	Int32Set.Remove(6);
+	Int32Set.Remove(8);
+	Int32Set.Remove(10);
+
+	Int32Set.Add(2);
+	Int32Set.Add(4);
+	Int32Set.Add(6);
+	Int32Set.Add(8);
+	Int32Set.Add(10);
+
+
+}
---
+++ Source/HelloUnreal/ContainerGameInstance.h
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "ContainerGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HELLOUNREAL_API UContainerGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+	
+public:
+
+	virtual void Init() override;
+
+	
+};
